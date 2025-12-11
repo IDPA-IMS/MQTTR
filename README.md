2c08719fb2934938839fc2399aa1dbfecac10832
Anon <206556099+An0n-01@users.noreply.github.com>
2025-12-11 20:49:40 +0100

    Adds initial project files
    Sets up the initial project structure for the MQTT receiver on a Raspberry Pi Pico W.
    
    This commit includes:
    - Initial `.gitignore` file
    - `README.md` with setup and usage instructions.
    - `env.example.py` for storing WiFi credentials
    - `main.py` containing the program's main entrypoint
    - `ssl.py` and `umqtt` as dependencies
    - `picozero` as a dependency

--- README.md
+++ README.md
@@ -9,6 +9,23 @@ This is a lightweight MQTT Reciever for a Raspberry Pi Pico W to control (in our
 > [!CAUTION]
 > This project is a work in progress and should be used with caution. Always test in a safe environment.
 
+## Expected Setup
+
+RC = "laptop"
+mqtt broker = "mosquitto"
+
+It is laid out (hard coded) that:
+- a mqtt broker is running on the laptop
+- the laptop is running a hotspot, and the Raspberry Pi Pico W is connected to the hotspot
+- the broker has `listener 1883 0.0.0.0` and `allow_anonymous true` in its config
+- firewall is disabled, or the laptop allows inbound connections on port 1883
+- the env.example.py file is renamed to env.py and the hotspot SSID and password are added
+
+## Usage
+
+0. Make sure you did the setup above.
+1. Upload all files from here directly to the Raspberry Pi Pico W and run the main.py file.
+
 ## **DISCLAIMER**
 
 **THIS PROJECT IS PROVIDED "AS IS" WITHOUT ANY WARRANTIES, EXPRESS OR IMPLIED. THE AUTHOR DISCLAIMS ALL WARRANTIES INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.**
---
+++ main.py
@@ -0,0 +1,94 @@
+import socket
+import network
+from time import sleep
+from picozero import pico_led
+import rp2
+import sys
+from lib.umqtt.simple import MQTTClient
+import env
+
+# WiFi credentials
+ssid = env.WIFI_SSID
+password = env.WIFI_PASSWORD
+
+print("[startup]: Starting MQTTR")
+
+# Exit if WiFi credentials are missing
+if ssid == "" or password == "":
+    print("[error]: Wifi SSID or password not set in env.py")
+    sys.exit()
+
+
+# Connect to WiFi
+def connect_to_hotspot():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    # Wait for connection
+    while not wlan.isconnected():
+        if rp2.bootsel_button() == 1:  # The connection process is cancelable
+            sys.exit()
+        print('[debug]: Waiting for connection...')
+        pico_led.on()  # Blinking LED when connecting
+        sleep(0.5)
+        pico_led.off()
+        sleep(0.5)
+
+    # Set MQTT broker as gateway
+    env.MQTT_BROKER = wlan.ifconfig()[2]
+    local_ip = wlan.ifconfig()[0]
+    print(f'[log]: Connected with pico ip: {local_ip}')
+    pico_led.on()
+    return local_ip
+
+
+# Test raw TCP connection to MQTT broker
+def test_connection_to_broker():
+    s = socket.socket()
+    try:
+        print("[test]: Testing raw TCP connection...")
+        s.settimeout(5)
+        s.connect((env.MQTT_BROKER, 1883))
+        print("[test]: TCP connection OK")
+    except Exception as e:
+        print("[error]: TCP connection failed:", e)
+        sys.exit()
+    finally:
+        s.close()
+
+
+# Callback function triggered when a message is received
+def message_handler(topic, msg):
+    msg = msg.decode()
+    print(f"[mqtt]: Received message on topic {topic}: {msg}")
+
+
+# === MAIN ENTRYPOINT ===
+connect_to_hotspot()            # connect to WiFi
+test_connection_to_broker()     # test connection to broker
+
+# Create MQTT client
+mqtt_client = MQTTClient(
+    client_id="MQTTRPICO-01",
+    server=env.MQTT_BROKER,
+    port=1883
+)
+
+mqtt_client.set_callback(message_handler)  # set callback function
+mqtt_client.connect()                      # connect to MQTT broker
+mqtt_client.subscribe(b"test/topic")       # subscribe to topic "test/topic"
+
+# Listen for incoming messages indefinitely
+while True:
+    if rp2.bootsel_button() == 1:
+        sleep(1)
+        if rp2.bootsel_button() == 1:
+            mqtt_client.disconnect()
+            print("[shutdown]: MQTTR shutdown")
+            sys.exit()
+        else:
+            continue
+    mqtt_client.check_msg()
+    sleep(0.1)
+
